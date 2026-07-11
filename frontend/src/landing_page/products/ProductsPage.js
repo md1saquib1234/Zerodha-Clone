47c996a73e57f70f94bc022c7005e67be2867d4c
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-11 20:31:38 +0530

    Revamp Hero and ProductsPage components with enhanced content and layout for improved user engagement

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Hero from './Hero';
+import LeftSection from './LeftSection';
+import RightSection from './RightSection';
+import Universe from './Universe';
 
 function ProductsPage() {
     return ( 
         <div>
-            <h2>Products</h2>
-            <p>This is the products section of the page.</p>
+            <Hero/>
+            <LeftSection />
+            <RightSection />
+            <Universe />
         </div>
      );
 }
