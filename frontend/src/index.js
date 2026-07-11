a340144ad4092a4c6ea0ef4ad6fb3b9b41492a9b
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-11 17:54:36 +0530

    Add NotFound component and update routing for 404 handling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,10 @@ import AboutPage from './landing_page/about/AboutPage';
 import ProductsPage from './landing_page/products/ProductsPage';
 import PricingPage from './landing_page/pricing/PricingPage';
 import SupportPage from './landing_page/support/SupportPage';
+import NotFound from './landing_page/NotFound';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -23,6 +25,8 @@ root.render(
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
+       <Route path="/*" element={<NotFound />} />
+
     </Routes>
       <Footer />
     
