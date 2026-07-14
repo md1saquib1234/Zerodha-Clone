c1d15e613aa7766d007c160deb4d45d96d761aa0
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-14 13:04:29 +0530

    Fix padding in RightSection component for improved layout

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -4,7 +4,7 @@ function RightSection({ imageURL, productName, productDesription, learnMore }) {
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-6 mt-5">
+        <div className="col-6 p-5 mt-5">
           <h1>{productName}</h1>
           <p>{productDesription}</p>
           <div>
